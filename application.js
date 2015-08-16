e01a217a142f3aa238cb6c7f68e0c8e5cb860175
Andy <andrew.sunglae.kim@gmail.com>
2015-08-16 18:16:13 -0400

    update module solution

--- application.js
+++ application.js
@@ -1,15 +1,11 @@
 var express = require("express")
 var app = express()
-var hbs = require("hbs")
-app.set('view engine', 'hbs')
-app.use(express.static(__dirname + '/public'))
+var routes = require("./routes")
 
-app.get("/:numberOfBottles?", function( req, res ){
-  var bottles = req.params.numberOfBottles || 99
-  var next = bottles - 1
-  res.render("index", {bottles: bottles, next: next})
-})
+app.set("view engine", "hbs")
+app.use(express.static(__dirname + "/public"))
+app.get("/:numBottles?", routes.index )
 
 app.listen(3000, function(){
-  console.log("app listening on port 3000")
+  console.log("listening on 3000")
 })
