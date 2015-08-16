3f21869b1f66749d859c62f947e1b192e335309a
Andy <andrew.sunglae.kim@gmail.com>
2015-08-16 17:28:29 -0400

    hbs solution

--- application.js
+++ application.js
@@ -1,20 +1,13 @@
 var express = require("express")
 var app = express()
+var hbs = require("hbs")
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
-
-app.get("/", function(req,res){
-  res.send("99 bottles of beer on the wall. <a href='/98'>Take one down pass it around</a>")
-})
 
 app.get("/:numberOfBottles?", function( req, res ){
   var bottles = req.params.numberOfBottles || 99
   var next = bottles - 1
-  if (bottles > 1){
-    res.send(bottles + " bottles of beer on the wall <a href='/" + next + "'>Take one down pass it around")
-  }
-  else{
-    res.send("1 bottle of beer on the wall <a href='/'>Start Over</a>")
-  }
+  res.render("index", {bottles: bottles, next: next})
 })
 
 app.listen(3000, function(){
